0c5da0cd6986fe156e052de6c5298cb5dae8d9d3
ErikHys <E.Hystad@student.uib.no>
2021-11-03 10:18:08 +0100

    keyframe

--- src/KeyFrame.cpp
+++ src/KeyFrame.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "KeyFrame.h"
+
+
+KeyFrame::KeyFrame(mat4 modelViewProjectionMatrix, vec2 viewPortSize, vec3 worldCameraPosition, vec3 worldLightPosition,
+                   vec3 lightIntensity, vec3 lightColor, vec3 backgroundColor, float explosionDist) {
+    this->modelViewProjectionMatrix = modelViewProjectionMatrix;
+    this->viewPortSize = viewPortSize;
+    this->worldCameraPosition = worldCameraPosition;
+    this->worldLightPosition = worldLightPosition;
+    this->lightIntensity = lightIntensity;
+    this->lightColor = lightColor;
+    this->backgroundColor = backgroundColor;
+    this->explosionDist = explosionDist;
+
+}
+
+mat4 KeyFrame::getModelViewProjectionMatrix() const {
+    return modelViewProjectionMatrix;
+}
+
+vec2 KeyFrame::getViewPortSize() const {
+    return viewPortSize;
+}
+
+vec3 KeyFrame::getWorldCameraPosition() const {
+    return worldCameraPosition;
+}
+
+vec3 KeyFrame::getWorldLightPosition() const {
+    return worldLightPosition;
+}
+
+vec3 KeyFrame::getLightIntensity() const {
+    return lightIntensity;
+}
+
+vec3 KeyFrame::getLightColor() const {
+    return lightColor;
+}
+
+vec3 KeyFrame::getBackgroundColor() const {
+    return backgroundColor;
+}
+
+float KeyFrame::getExplosionDist() const {
+    return explosionDist;
+}
--- src/KeyFrame.h
+++ src/KeyFrame.h
@@ -1,12 +1,51 @@
 //
 // Created by erik_ on 03.11.2021.
 //
+#include "ModelRenderer.h"
+#include <globjects/base/File.h>
+#include <globjects/State.h>
+#include <iostream>
+#include <filesystem>
+#include <imgui.h>
+#include "Viewer.h"
+#include "Scene.h"
+#include "Model.h"
+#include <sstream>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
 
 #ifndef MINITY_KEYFRAME_H
 #define MINITY_KEYFRAME_H
 
+using namespace glm;
 
 class KeyFrame {
+
+public:
+    KeyFrame(mat4 modelViewProjectionMatrix, vec2 viewPortSize, vec3 worldCameraPosition, vec3 WorldLightPosition,
+             vec3 lightIntensity, vec3 lightColor, vec3 backgroundColor, float explosionDist);
+    mat4 getModelViewProjectionMatrix() const;
+    vec2 getViewPortSize() const;
+    vec3 getWorldCameraPosition() const;
+    vec3 getWorldLightPosition() const;
+    vec3 getLightIntensity() const;
+    vec3 getLightColor() const;
+    vec3 getBackgroundColor() const;
+    float getExplosionDist() const;
+
+private:
+    mat4 modelViewProjectionMatrix;
+    vec2 viewPortSize;
+    vec3 worldCameraPosition;
+    vec3 worldLightPosition;
+    vec3 lightIntensity;
+    vec3 lightColor;
+    vec3 backgroundColor;
+    float explosionDist;
 
 };
 
